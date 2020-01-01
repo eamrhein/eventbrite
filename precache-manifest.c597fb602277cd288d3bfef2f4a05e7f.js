1c871200d3a9424a90aa89a55c3cb9fd8cf1a6d0
Eric Amrhein <eamrhein@protonmail.ch>
2020-01-01 13:02:38 -0800

    Updates

---
+++ precache-manifest.c597fb602277cd288d3bfef2f4a05e7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60f990987cdee16bfb03a803ec09f4f",
+    "url": "/eventbrite/index.html"
+  },
+  {
+    "revision": "90e693bca6d97792ee88",
+    "url": "/eventbrite/static/css/2.b60ef663.chunk.css"
+  },
+  {
+    "revision": "dbb848ac2b58f647dd8b",
+    "url": "/eventbrite/static/css/main.32479651.chunk.css"
+  },
+  {
+    "revision": "90e693bca6d97792ee88",
+    "url": "/eventbrite/static/js/2.26bbf683.chunk.js"
+  },
+  {
+    "revision": "801eb6b718cfeb49e4699ab80a85e8cf",
+    "url": "/eventbrite/static/js/2.26bbf683.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbb848ac2b58f647dd8b",
+    "url": "/eventbrite/static/js/main.71040439.chunk.js"
+  },
+  {
+    "revision": "301f0b89acfb752d0b00",
+    "url": "/eventbrite/static/js/runtime-main.e330a47c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/eventbrite/precache-manifest.8d2f8c6d8082e231195c285f215fd6e7.js"
+  "/eventbrite/precache-manifest.c597fb602277cd288d3bfef2f4a05e7f.js"
 );
 
 self.addEventListener('message', (event) => {
